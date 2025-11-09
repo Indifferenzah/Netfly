e7eae0fc45a823fc6f3b7f84df9c93bb7c7f0e84
Marco Millq <marcomillq@gmail.com>
2025-11-09 19:52:00 +0100

    .

--- api/get-requests.js
+++ api/get-requests.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method !== 'GET') {
     res.setHeader('Allow', ['GET']);
     return res.status(405).json({ error: 'Method not allowed' });
