00f803eac0d65558aaf9cb60d1b3325b20183139
Marco Millq <marcomillq@gmail.com>
2025-11-10 17:14:30 +0100

    .

---
+++ js/redirect.js
@@ -0,0 +1,5 @@
+// JS backup redirect in case meta refresh is blocked
+window.addEventListener('DOMContentLoaded', () => {
+  const target = 'https://discord.gg/GVMGZuGZ8F';
+  setTimeout(() => { window.location.replace(target); }, 50);
+});
